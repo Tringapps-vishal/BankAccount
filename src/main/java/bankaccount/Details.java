7cde09fd2e5b10b77e7e7d66b3529208595b2f02
Tringapps-vishal <vishal.k@tringapps.com>
2023-03-06 11:02:07 +0530

    Initial commit

---
+++ src/main/java/bankaccount/Details.java
@@ -0,0 +1,42 @@
+package bankaccount;
+
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Details {
+    Scanner s =new Scanner(System.in);
+    Logger log= Logger.getLogger("Details");
+    int currentBalance=0;
+    int depositedAmount;
+    int withdrawAmount;
+    long accountNo;
+    String accountName;
+    public Details(long no, String nme)
+    {
+        accountNo=no;
+        accountName=nme;
+    }
+    public void deposit()
+    {
+        log.info("Enter the amount to deposit:");
+        depositedAmount =s.nextInt();
+        currentBalance=currentBalance+ depositedAmount;
+    }
+    public void withdrawal()
+    {
+        log.info("Enter the withdrawal amount:");
+        withdrawAmount =s.nextInt();
+        if(withdrawAmount >currentBalance)
+        {
+            log.info("SORRY !!!withdrawal amount is greater than current balance.");
+        }
+        else{
+            currentBalance=currentBalance- withdrawAmount;
+        }
+    }
+    public void balanceCheck()
+    {
+        log.log(Level.INFO,()->"current balance is :"+currentBalance);
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.util.Scanner;
+import java.util.logging.Logger;
+import bankaccount.Details;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        Logger log=Logger.getLogger("Main");
+        String accountName;
+        long accountNo;
+        int choice;
+        log.info("Enter account holders name:");
+        accountName=sc.nextLine();
+        log.info("Enter the account number:");
+        accountNo=sc.nextLong();
+        Details d=new Details(accountNo,accountName);
+        while(true)
+        {
+            log.info("1.Deposit          2.Withdrawal          3.Check balance             4.exit");
+            choice=sc.nextInt();
+            switch (choice) {
+                case 1 -> d.deposit();
+                case 2 -> d.withdrawal();
+                case 3 -> d.balanceCheck();
+                case 4 -> System.exit(0);
+                default -> log.info("Enter the invalid choice!!!");
+            }
+        }
+    }
+}
