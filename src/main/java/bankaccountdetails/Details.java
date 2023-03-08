e0b40cdc937ed05858244c6cbc51e4fcdf1e9fce
Tringapps-vishal <vishal.k@tringapps.com>
2023-03-08 12:28:30 +0530

    commit

--- src/main/java/bankaccountdetails/Details.java
+++ src/main/java/bankaccountdetails/Details.java
@@ -1,41 +1,47 @@
 package bankaccountdetails;
 
+
 import java.util.Scanner;
+import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 public class Details {
+    public HashMap<Long,Details> map=new HashMap<>();
     protected Scanner sc =new Scanner(System.in);
     Logger log= Logger.getLogger("Details");
-    private int currentBalance=0;
+    private Details currentBalance=0;
     private int depositedAmount;
     private int withdrawAmount;
 
     long accountNo;
     String accountName;
-    public Details(long no, String name)
+    double balance;
+    public Details(long no, String name,double balance)
     {
         accountNo=no;
         accountName=name;
+        this.balance=balance;
         log.info("Account Created Successfully!! with the Account Number "+accountNo);
     }
     public void deposit()
     {
         log.info("Enter the Account number:");
         long tempAccountNumber=sc.nextLong();
-        if(tempAccountNumber==accountNo) {
+        if(map.containsKey(tempAccountNumber)) {
             log.info("Enter the amount to deposit:");
             depositedAmount = sc.nextInt();
+            map.get(tempAccountNumber)=currentBalance;
             currentBalance = currentBalance + depositedAmount;
             log.log(Level.INFO, () -> depositedAmount + " has been deposited successfully!!!");
         }
         else
-            log.info("Wrong Account number!!");
+            log.info("Account not found!!");
     }
     public void withdrawal()
     {
         log.info("Enter the Account number:");
         long tempAccountNumber=sc.nextLong();
-        if(tempAccountNumber==accountNo) {
+        if(map.containsKey(tempAccountNumber)) {
         log.info("Enter the withdrawal amount:");
         withdrawAmount =sc.nextInt();
         if(withdrawAmount >currentBalance)
@@ -46,15 +52,15 @@ public class Details {
             currentBalance=currentBalance- withdrawAmount;
             log.log(Level.INFO,()->withdrawAmount+" has been withdrawn successfully!!!");
         }}else
-            log.info("Wrong Account number!!");
+            log.info("Account not found!!");
     }
     public void balanceCheck()
     {   log.info("Enter the Account number:");
         long tempAccountNumber=sc.nextLong();
-        if(tempAccountNumber==accountNo) {
+        if(map.containsKey(tempAccountNumber)) {
             log.log(Level.INFO, () -> "current balance is :" + currentBalance);
         }
         else
-            log.info("Wrong Account number!!");
+            log.info("Account not found!!");
     }
 }
--- src/main/java/bankaccountgetdata/GetData.java
+++ src/main/java/bankaccountgetdata/GetData.java
@@ -1,11 +1,12 @@
 package bankaccountgetdata;
 
 import bankaccountdetails.Details;
-
+import java.util.HashMap;
 import java.util.Scanner;
 import java.util.logging.Logger;
 
 public class GetData {
+
     public void getdata()
     {
         Scanner sc=new Scanner(System.in);
@@ -17,7 +18,11 @@ public class GetData {
         accountName=sc.nextLine();
         log.info("Enter the account number:");
         accountNo=sc.nextLong();
-        Details d=new Details(accountNo,accountName);
+        double balance=0;
+        HashMap<Long,Double> map=new HashMap<>();
+        Details d=new Details(accountNo,accountName,balance);
+        map.put(accountNo,balance);
+
         while(true)
         {
             log.info("1.Deposit     2.Withdrawal       3.Check balance      4.Add Account       5.Exit");
