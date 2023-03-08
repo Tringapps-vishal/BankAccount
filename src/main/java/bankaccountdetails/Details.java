1abc949bebb56cc0294bbe428ae605dc20f5cc20
Tringapps-vishal <vishal.k@tringapps.com>
2023-03-08 13:11:12 +0530

    commit

--- src/main/java/bankaccountdetails/Details.java
+++ src/main/java/bankaccountdetails/Details.java
@@ -2,13 +2,16 @@ package bankaccountdetails;
 
 
 import bankaccountgetdata.GetData;
+
+import java.util.HashMap;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 public class Details extends GetData {
     protected Scanner sc =new Scanner(System.in);
     Logger log= Logger.getLogger("Details");
-    private double currentBalance=0;
+    private HashMap<Long,Long> map=new HashMap<>();
+    private long currentBalance=0;
     private final long accountNo;
     private final  String accountName;
     private double balance;
@@ -62,5 +65,38 @@ public class Details extends GetData {
         }
         else
             log.info("Account not found!!");
+    }
+    public void getData()
+    {
+        Scanner sc=new Scanner(System.in);
+        Logger log=Logger.getLogger("Main");
+        String accountName;
+        long accountNo;
+        int choice;
+        log.info("Enter account holders name:");
+        accountName=sc.nextLine();
+        log.info("Enter the account number:");
+        accountNo=sc.nextLong();
+        long balance=0;
+
+        Details d=new Details(accountNo,accountName,balance);
+        map.put(accountNo,balance);
+
+        while(true)
+        {
+            log.info("1.Deposit     2.Withdrawal       3.Check balance      4.Add Account       5.Exit");
+            choice=sc.nextInt();
+            switch (choice) {
+                case 1 -> d.deposit();
+                case 2 -> d.withdrawal();
+                case 3 -> d.balanceCheck();
+                case 4 -> getData();
+                case 5 -> {
+                    log.info("Exiting!!!");
+                    System.exit(0);
+                }
+                default -> log.info("Enter the invalid choice!!!");
+            }
+        }
     }
 }
